360b0c9f9f7d4d8b55794493e96d04adec4237df
Deependra Singh <deependras@iitg.ac.in>
2025-12-16 17:17:53 +0530

    updated a few things

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+            const faqItems = document.querySelectorAll('.faq-item');
+            faqItems.forEach(item => {
+                item.addEventListener('click', () => {
+                    item.classList.toggle('open');
+                });
+            });
+        });
